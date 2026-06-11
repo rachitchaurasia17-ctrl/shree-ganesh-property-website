642f3f979bef5aa35eade6817695e61c67eddf64
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 16:00:42 +0530

    Fix Realty suffix and update correct address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for Shree Gane
 
 Firm facts:
 - Firm: Shree Ganesh Property
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: Main Kharar-Landran Road corridor, Mohali
 - Phone: +91 XXXXX XXXXX
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
